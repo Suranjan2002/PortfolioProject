68c8e1245125e6a53a8d9cee4a25ad9603b612be
Sayak Karar <sayakarar@gmail.com>
2021-07-24 01:46:26 +0530

    Added testimonials

--- templates/main.js
+++ templates/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*ACHIEVEMENT SWIPER*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -104,4 +104,22 @@ let swiper = new Swiper('.portfolio__container', {
         el: '.swiper-pagination',
         clickable: true,
     },
+});
+
+/*TESTIMONIAL SWIPER*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets:true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
 });
