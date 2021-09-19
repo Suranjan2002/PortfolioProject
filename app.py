dbdddec8e15f76b7112bf565452e1a9ae9937f78
Sayak Karar <sayakarar@gmail.com>
2021-09-19 10:32:41 +0530

    Changed db route and table contents; Required input fields

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lkrigVe1qr:BB0pvHmkF0@remotemysql.com/lkrigVe1qr'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myremotesqldb:password@db4free.net/myremotesqldb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class contact(db.Model):
-    name = db.Column(db.String(200), nullable=True)
-    email = db.Column(db.String(500), primary_key=True)
-    message = db.Column(db.String(2500), nullable=True)
-    time = db.Column(db.DateTime, default=datetime.now())
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=False, nullable=False)
+    email = db.Column(db.String(200), unique=False, nullable=False)
+    message = db.Column(db.String(1000), unique=False, nullable=False)
+    datetime = db.Column(db.DateTime, default=datetime.now())
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -20,21 +21,20 @@ def index():
         name1 = request.form['name']
         email1 = request.form['email']
         message1 = request.form['message']
-        date_time1 = datetime.now()
+        datetime1 = datetime.now()
 
         info = contact(
             name = name1,
             email = email1,
             message = message1,
-            time = date_time1
+            datetime = datetime1
         )
         db.session.add(info)
         db.session.commit()
         
         return render_template('thankyou.html')
 
-    info1 = contact.query.all()
-    return render_template('index.html', info=info1)
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True, port=2581)
